46fa25260bd37d35dfceefbede56cdf6f31f37cc
Julien Marrec <julien.marrec@gmail.com>
2026-05-18 17:46:26 +0200

    Fake having generateiddfactory

---
+++ src/generateiddfactory/IddFactory.hxx
@@ -0,0 +1,16 @@
+/***********************************************************************************************************************
+*  OpenStudio(R), Copyright (c) Alliance for Energy Innovation, LLC.
+*  See also https://openstudio.net/license
+***********************************************************************************************************************/
+
+#ifndef UTILITIES_IDD_IDDFACTORY_HXX
+#define UTILITIES_IDD_IDDFACTORY_HXX
+
+#include <utilities/UtilitiesAPI.hpp>
+
+#include <utilities/idd/IddEnums.hpp>
+#include <utilities/idd/IddEnums.hxx>
+
+namespace openstudio {}  // namespace openstudio
+
+#endif  //UTILITIES_IDD_IDDFACTORY_HXX
